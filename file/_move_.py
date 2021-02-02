3bd739daa01a3391f8f545c51b86450f27f14044
xiaokexiang <xxiaokexiang@gmail.com>
2021-02-02 17:10:29 +0800

    文件夹移动 & 复制相关

---
+++ file/_move_.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import time
+
+"""
+将当前目录下的文件或文件夹内的文件（包含子文件夹）保存到指定目录
+"""
+
+
+def _move_():
+    source = input('Please input source dir: ')
+    dest = input('Please input dest dir: ')
+    log_name = os.path.join(dest, 'move.log')
+    f = open(log_name, mode='a')
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    count = 0
+    for root, dirs, files in os.walk(source, topdown=False):
+        for filename in files:
+            if filename != '_move_.py':
+                count = count + 1
+                cp_source = os.path.join(root, filename)
+                if os.path.exists(os.path.join(dest, filename)):  # 判断文件名是否重复
+                    cp_dest = os.path.join(dest, 'DUP_' + filename)
+                    print('No: ' + str(count) + '; source: ' + cp_source + ', dest: ' + cp_dest, file=f)
+                    shutil.move(cp_source, cp_dest)
+                else:
+                    cp_dest = os.path.join(dest, filename)
+                    print('No: ' + str(count) + '; source: ' + cp_source + ', dest: ' + cp_dest, file=f)
+                    shutil.move(cp_source, cp_dest)
+    f.close()
+
+
+_move_()
---
+++ file/_rename_.py
@@ -0,0 +1,30 @@
+"""
+判断图片的拍摄时间，并拷贝及重命名
+"""
+import os
+import shutil
+import time
+
+
+def _rename_to_():
+    source = input('Please input source dir: ')
+    dest = input('Please input dest dir: ')
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    count = 0
+    for root, dirs, files in os.walk(source, topdown=False):
+        for filename in files:
+            count = count + 1
+            image_time = time.localtime(os.stat(os.path.join(root, filename)).st_mtime)
+            dir_time = time.strftime('%Y_%m', image_time)
+            suffix = filename.split('.')[1]
+            create_time = time.strftime('%Y_%m_%d_%H_%M_%S', image_time)
+            new_file_name = create_time + '.' + suffix
+            print('No: ' + str(count) + ', origin_name: ' + filename + ', new_name: ' + new_file_name)
+            dest_dir = os.path.join(dest, dir_time + '\\')
+            if not os.path.isdir(dest_dir):
+                os.makedirs(dest_dir)
+            shutil.copy(os.path.join(root, filename), os.path.join(dest_dir, new_file_name))
+
+
+_rename_to_()
